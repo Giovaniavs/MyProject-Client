6948cd642df3bcb8fdce43b18ebfba7d4b6784ef
Giovaniavs <gavs2@cin.ufpe.br>
2021-02-18 10:21:48 -0300

    Feature(main): base of the react app created

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { Home } from './views';
+
+const App: React.FC = () => (
+  <Home />
+);
+
+
+export default App;
---
+++ src/views/Home/Home.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Home: React.FC = () => (
+  <>
+    <h1>First Component</h1>
+  </>
+);
+
+export default Home;
