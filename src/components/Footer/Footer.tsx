ac067be935ab5b835281f050ffb89ea048beb908
Giovaniavs <gavs2@cin.ufpe.br>
2021-02-26 19:51:39 -0300

    Feature(mobile): footer and navbar configured to fit in the mobile version

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -5,11 +5,13 @@ import {
   ImageComponent,
 } from '../../global/globalStyle';
 
+import { FooterContainer } from './styles';
+
 function Footer() {
   return (
     <>
-      <FlexContainer height="50px" align="center" boxShadow="0 3px 32px rgba(0,0,0,.16)">
-        <FlexContainer justify="flex-start" margin="0 2rem">
+      <FooterContainer height="50px" align="center" boxShadow="0 3px 32px rgba(0,0,0,.16)">
+        <FlexContainer className="social-network-icons" justify="flex-start" margin="0 2rem">
           <a href="https://www.instagram.com/giov_albuquerque/">
             <ImageComponent width="40px" margin="0 10px" alt="instagram icon" src="https://img.icons8.com/plasticine/48/000000/instagram.png" />
           </a>
@@ -21,14 +23,14 @@ function Footer() {
           </a>
         </FlexContainer>
 
-        <FlexContainer align="center" justify="flex-end" padding="0 20px 0 0">
+        <FlexContainer className="message-footer" align="center" justify="flex-end" padding="0 20px 0 0">
           <H2 width="fit-content" weight="500" textSize="1.2em"> Made with </H2>
           <ImageComponent width="40px" margin="0 5px" alt="react icon" src="https://img.icons8.com/clouds/100/000000/react.png" />
           <H2 width="fit-content" weight="500" textSize="1.2em"> and </H2>
           <ImageComponent width="50px" alt="heart icon" src="https://img.icons8.com/clouds/100/000000/like--v1.png" />
-          <H2 width="fit-content" weight="500" textSize="1.2em"> by Giovani! </H2>
+          <H2 width="fit-content" weight="500" textSize="1.2em"> by Giovani ^^ </H2>
         </FlexContainer>
-      </FlexContainer>
+      </FooterContainer>
     </>
 
   );
