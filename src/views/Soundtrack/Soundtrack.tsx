53476a53616bf6cae97626324464f024cf85f648
Giovaniavs <gavs2@cin.ufpe.br>
2021-03-03 21:37:53 -0300

    Hotfix(soundtrack): increased the value of smaller screens variable

--- src/views/Soundtrack/Soundtrack.tsx
+++ src/views/Soundtrack/Soundtrack.tsx
@@ -10,7 +10,7 @@ import { SoundtrackContainer } from './styles';
 
 function Soundtrack() {
   const [cardHeight, setCardHeight] = useState(380);
-  const smallerScreen = 720;
+  const smallerScreen = 850;
   const currentScreen = screen.height;
 
   useEffect(() => {
